e6470e792302e1ff0e54be4d52d69256a8efb6f5
timothybagas <63138796+timothybagas@users.noreply.github.com>
2022-11-12 05:07:12 +0700

    create onDelete handler

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Button, IconButton, List, Paper, Typography } from "@mui/material";
+import { List } from "@mui/material";
 import ListItemUser from "./components/ListItemUser";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { AddCircle } from "@mui/icons-material";
-import logo from './logo.svg';
 import './App.css';
 
 const BASE_API_URL = 'https://jsonplaceholder.typicode.com';
 
-
-function App() {
-  
+function App() {  
   const [data, setData] = useState([]);
-
 
   useEffect(() => {
     async function getData() {
@@ -35,19 +30,37 @@ function App() {
     }
 
     getData();
-  }, );
+  }, []);
+
+  const handleDeletePost = (postID, idx) => {
+    (async function() {
+      await axios
+        .delete(`${BASE_API_URL}/posts/${postID}`)
+        .then(_ => {
+          let arr = data;
+          arr.splice(idx, 1);
+          console.log(arr);
+          setData([...arr]);
+        })
+        .catch(err => {
+          console.log(err);
+          window.alert(err);
+        });
+    })()
+  };
   
   return (
     <div className="App">
         <div className="list-container">
           <div className="list-title-wrapper">
             <List>
-              {data.map((d) => (
+              {data.map((d, idx) => (
                 <ListItemUser
-              key={d.id}
-              primaryText={d.title}
-              secondaryText={d.body}
-              />
+                  key={d.id}
+                  primaryText={d.title}
+                  secondaryText={d.body}
+                  onDelete={() => handleDeletePost(d.id, idx)}
+                />
               ))}
             </List>
           </div>
