25a8d7683eaaaea50f99ca123687cd5a1c286501
Apriyandro Triandito Kusumo <63110423+pizzabungkus@users.noreply.github.com>
2022-11-12 22:52:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { List } from "@mui/material";
 import ListItemUser from "./components/ListItemUser";
-import { useEffect, useState } from "react";
+import CreatePost from "./components/CreatePost";
+import { useEffect, useState, createContext } from "react";
 import axios from "axios";
 import './App.css';
+import UpdatePost from "./components/UpdatePost";
 
 const BASE_API_URL = 'https://jsonplaceholder.typicode.com';
+
+export const UpdateContext = createContext();
 
 function App() {  
   const [data, setData] = useState([]);
@@ -48,10 +52,58 @@ function App() {
         });
     })()
   };
+
+  const handleCreatePost = (title, body) => {
+    (async function() {
+      await axios
+        .post(`${BASE_API_URL}/posts`, {
+          title: title,
+          body: body,
+          userId: 1,
+        })
+        .then(res => {
+          console.log(res);
+          const post = res.data;
+          setData([post, ...data]);
+        })
+        .catch(err => {
+          console.log(err);
+          window.alert(err);
+        });
+    })()
+  };
+
+  const [post, setPost] = useState(null);
+
+  const handleUpdatePost = (title, body) => {
+    const { id, idx } = post;
+    (async function() {
+      await axios
+        .put(`${BASE_API_URL}/posts/${id}`, {
+          title: title,
+          body: body,
+        })
+        .then(res => {
+          console.log(res);
+          let arr = data;
+          arr[idx] = res.data;
+          setData([...arr]);
+        })
+        .catch(err => {
+          console.log(err);
+          window.alert(err);
+        });
+    })()
+  };
+
+  
+
+
   
   return (
     <div className="App">
         <div className="list-container">
+          <CreatePost onSubmit={handleCreatePost} />
           <div className="list-title-wrapper">
             <List>
               {data.map((d, idx) => (
@@ -60,11 +112,18 @@ function App() {
                   primaryText={d.title}
                   secondaryText={d.body}
                   onDelete={() => handleDeletePost(d.id, idx)}
+                  onUpdate={() => setPost({...d, idx})}
                 />
               ))}
             </List>
           </div>
         </div>
+        <UpdateContext.Provider value={{ post, setPost }}>
+          <UpdatePost
+            onUpdate={handleUpdatePost}
+            post={{ postTitle: post?.title, postBody: post?.body }}
+          />
+        </UpdateContext.Provider>
     </div>
   );
 }
